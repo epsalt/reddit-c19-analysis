5623e0777637cad578819c8049e4115315d8a63d
Evan Saltman <evan.saltman@gmail.com>
2020-04-27 16:07:00 -0600

    changed analysis to calculate frequency of keyword groups

--- analysis.py
+++ analysis.py
@@ -7,25 +7,26 @@ import dask
 import dask.bag as db
 import dask.dataframe as dd
 import pandas as pd
+import yaml
 
 
-def kws(record, props, pattern):
-    """Assign keywords and drop columns"""
-    text = (record[prop] for prop in props if record.get(prop))
-    matches = re.findall(pattern, "".join(text), flags=re.IGNORECASE)
-    keywords = set(kw.lower() for kw in matches)
+def kws(record, props, patterns):
+    """Assign categories and drop columns"""
+    text = "".join(record[prop] for prop in props if record.get(prop))
+    categories = []
 
-    if not keywords:
-        keywords = ["nokeyword"]
+    for category, pattern in patterns.items():
+        if re.findall(pattern, text, flags=re.IGNORECASE):
+            categories.append(category)
 
     return {
         "created_utc": record["created_utc"],
         "subreddit": record["subreddit"],
-        "keyword": list(keywords),
+        "cat": categories,
     }
 
 
-def process(jsonl, pattern, props):
+def process(jsonl, patterns, props):
     """Subreddit data processing pipeline.
 
     Use bag for initial keyword calculation and filtering, was getting
@@ -35,41 +36,47 @@ def process(jsonl, pattern, props):
     """
 
     bag = db.read_text(jsonl, blocksize="10MiB").map(json.loads)
-    df = bag.map(kws, props=props, pattern=pattern).to_dataframe()
+    df = bag.map(kws, props, patterns).to_dataframe()
 
     df["created_utc"] = dd.to_datetime(df["created_utc"], unit="s")
     df = df.set_index("created_utc")
 
-    df = df.explode("keyword")
-    df["keyword"] = df["keyword"].astype("category").cat.as_known()
+    counts = df.groupby([pd.Grouper(freq="1d"), "subreddit"]).size().to_frame("totals")
+
+    df = df.explode("cat")
+    df["cat"] = df["cat"].fillna("nocat").astype("category").cat.as_known()
 
     agg = (
-        df.groupby([pd.Grouper(freq="1d"), "subreddit", "keyword"])
+        df.groupby([pd.Grouper(freq="1d"), "subreddit", "cat"])
         .size()
         .to_frame("count")
         .reset_index()
+        .join(counts, on=["created_utc", "subreddit"])
     )
+
+    agg["freq"] = agg["count"] / agg["totals"]
 
     return agg
 
 
 def main(termf, subsf, comsf, subprops, comprops, subsout, comsout):
     """Run everything, change dask config if multiprocessing is causing problems"""
+
     with open(termf) as f:
-        terms = [term.strip() for term in f.readlines()]
-        pattern = "|".join(terms)
+        terms = yaml.load(f, Loader=yaml.Loader)
+        patterns = {cat: "|".join(kw) for cat, kw in terms.items()}
 
     with dask.config.set(scheduler="processes"):
-        subs = process(subsf, pattern, subprops)
+        subs = process(subsf, patterns, subprops)
         subs.to_csv(subsout, index=False, single_file=True)
 
-        coms = process(comsf, pattern, comprops)
+        coms = process(comsf, patterns, comprops)
         coms.to_csv(comsout, index=False, single_file=True)
 
 
 if __name__ == """__main__""":
     main(
-        termf="config/coronaterms.txt",
+        termf="config/coronaterms.yaml",
         subsf="data/subs.jsonl",
         comsf="data/coms.jsonl",
         subprops=["title", "selftext"],
